9cb9220adeffd959fcdf82c076855e6789a77f2d
Logan Boswell <loganstuartboswell@gmail.com>
2024-11-25 21:10:45 -0600

    Got manual_explore.launch.xml to launch simulate.launch.xml from the nubot package

--- nubot_nav/setup.py
+++ nubot_nav/setup.py
@@ -4,19 +4,20 @@ package_name = 'nubot_nav'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/manual_explore.launch.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='lbos7',
+    maintainer='Logan Boswell',
     maintainer_email='loganstuartboswell@gmail.com',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='GPLv3',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
