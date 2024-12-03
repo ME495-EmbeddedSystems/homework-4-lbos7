5a46e0f10d93f141764d61c27459f6a4b8c08abe
Logan Boswell <loganstuartboswell@gmail.com>
2024-12-02 22:03:37 -0600

    Added docstrings

--- nubot_nav/nubot_nav/explore.py
+++ nubot_nav/nubot_nav/explore.py
@@ -1,3 +1,5 @@
+"""A Node for controlling the a differential drive robot to map an area."""
+
 import rclpy
 from rclpy.node import Node
 
@@ -11,21 +13,50 @@ import random
 
 import math
 
+
 class State(Enum):
-    """ Current state of the system.
-        Determines what the main timer function should be doing on each iteration
     """
+    Current state of the system.
+
+    Determines what the main timer function should be doing on each
+    iteration
+    """
+
     STOPPED = auto(),
     MOVING = auto()
 
+
 class Explore(Node):
+    """
+    Node for controlling the diff-drive robot to explore an environment.
+
+    Subscribes
+    ----------
+    map : nav_msgs/msg/OccupancyGrid - the map of the area
+    cmd_vel : geometry_msgs/msg/Twist - the speed the robot
+
+    Publishes
+    ---------
+    goal_pose : geometry_msgs/msg/PoseStamped - where the robot should move
+
+    """
 
     def __init__(self):
+        """Initialize the explore node."""
         super().__init__('explore')
         self.tmr = self.create_timer(1/100, self.timer_callback)
-        self.map_sub = self.create_subscription(OccupancyGrid, 'map', self.map_callback, 10)
-        self.cmd_vel_sub = self.create_subscription(Twist, 'cmd_vel', self.vel_callback, 10)
+
+        self.map_sub = self.create_subscription(OccupancyGrid,
+                                                'map',
+                                                self.map_callback,
+                                                10)
+        self.cmd_vel_sub = self.create_subscription(Twist,
+                                                    'cmd_vel',
+                                                    self.vel_callback,
+                                                    10)
+
         self.goal_pub = self.create_publisher(PoseStamped, 'goal_pose', 10)
+
         self.map = None
         self.map_width = 0
         self.map_height = 0
@@ -38,6 +69,12 @@ class Explore(Node):
         self.state = State.STOPPED
 
     def timer_callback(self):
+        """
+        Timer callback for controlling the diff-drive robot.
+
+        Publishes commands at a set frequency
+
+        """
         if (self.state == State.STOPPED) and (self.map != None):
             index = random.randint(0, len(self.map.data) - 1)
             if self.map.data[index] == -1:
@@ -55,15 +92,19 @@ class Explore(Node):
                 new_y = self.map_min_y + new_y_cell * self.map_res
                 goalpose.pose.position.x = new_x
                 goalpose.pose.position.y = new_y
-                # angle = math.atan2(self.map_min_y - new_y, self.map_min_x - new_x)
-                # goalpose.pose.orientation.w = math.cos(angle * .5)
-                # goalpose.pose.orientation.z = math.sin(angle * .5)
                 self.goal_pub.publish(goalpose)
-        # elif self.state == State.MOVING:
-        #     pass
-        # pass
 
     def map_callback(self, map):
+        """
+        Update the variables associated with the map.
+
+        Updates variables relating to the current map
+
+        Args:
+        ----
+        map : (nav_msgs/msg/OccupancyGrid) - the current map
+
+        """
         self.map = map
         self.map_width = map.info.width
         self.map_height = map.info.height
@@ -74,10 +115,21 @@ class Explore(Node):
         self.map_max_y = self.map_min_y + self.map_height * self.map_res
 
     def vel_callback(self, twist):
+        """
+        Monitor the commanded velocity.
+
+        Updates the current state accordingly
+
+        Args:
+        ----
+        twist : (geometry_mgs/msg/Twist) - the commanded velocity
+
+        """
         if twist.linear.x == 0.0 and twist.angular.z == 0.0:
             self.state = State.STOPPED
         else:
             self.state = State.MOVING
+
 
 def main(args=None):
     """Entrypoint for the explore node."""
